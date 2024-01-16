a7974b8885b723a461e2fa555513b992ecef66f6
Vital <98259769+ydm20231608@users.noreply.github.com>
2024-01-16 15:08:09 +0700

    Update model.py

--- model.py
+++ model.py
@@ -10,24 +10,7 @@ generation_config = {
     "top_k": 1,
     "max_output_tokens": 50,
 }
-# safety_settings = [
-#   {
-#     "category": "HARM_CATEGORY_HARASSMENT",
-#     "threshold": "BLOCK_NONE"
-#   },
-#   {
-#     "category": "HARM_CATEGORY_HATE_SPEECH",
-#     "threshold": "BLOCK_NONE"
-#   },
-#   {
-#     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-#     "threshold": "BLOCK_NONE"
-#   },
-#   {
-#     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-#     "threshold": "BLOCK_NONE"
-#   },
-# ]
+
 
 safety_settings = {
     "HARM_CATEGORY_HARASSMENT": "BLOCK_NONE",
@@ -46,7 +29,6 @@ model = genai.GenerativeModel(
 
 
 model_vision = genai.GenerativeModel('gemini-pro-vision',
-                                    #  generation_config=generation_config,
                                      safety_settings=safety_settings)
 
 chat = model.start_chat()
